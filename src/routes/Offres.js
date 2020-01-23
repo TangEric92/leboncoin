ffbf59418c4c83f938207d055ace752000006980
Eric <you@example.com>
2020-01-23 19:51:33 +0100

    add pagination

--- src/routes/Offres.js
+++ src/routes/Offres.js
@@ -3,11 +3,25 @@ import Annonce from "../components/Annonce";
 import axios from "axios";
 function Offres() {
   const [annonces, setAnnonces] = useState([]);
+  //skip = page * limit
+  const [page, setPage] = useState(0);
+  const [data, setData] = useState(null);
+  const limit = 25;
+
+  const skip = page * limit;
+  let count = 0;
+  let res = null;
   const fetchAnnonces = () => {
     axios
-      .get("https://leboncoin-api.herokuapp.com/api/offer/with-count")
+      .get(
+        `https://leboncoin-api.herokuapp.com/api/offer/with-count?skip=${skip}&limit=${limit}`
+      )
       .then(response => {
+        setData(response.data);
+
         setAnnonces(response.data.offers);
+
+        count = response.data.count / 25;
       })
       .catch(err => {
         console.log(err);
@@ -16,7 +30,22 @@ function Offres() {
 
   useEffect(() => {
     fetchAnnonces();
-  }, []);
+  }, [page]);
+
+  const setNewPage = nbr => {
+    setPage(nbr);
+  };
+  const generatePagination = () => {
+    const elem = [];
+    for (let i = 0; i < data.count / limit; i++) {
+      elem.push(
+        <button onClick={() => setNewPage(i)} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return elem;
+  };
 
   return (
     <div className="offres">
@@ -29,6 +58,7 @@ function Offres() {
           />
         );
       })}
+      {data && generatePagination()}
     </div>
   );
 }
